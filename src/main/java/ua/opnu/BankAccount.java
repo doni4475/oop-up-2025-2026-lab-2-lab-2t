47d8b11d3e333cf635f21a111ead441dcf3a0461
Anton Savienkov <9145565@stud.op.edu.ua>
2025-10-16 23:01:49 +0200

    Lab2

--- src/main/java/ua/opnu/BankAccount.java
+++ src/main/java/ua/opnu/BankAccount.java
@@ -1,27 +1,39 @@
 package ua.opnu;
 
 public class BankAccount {
-    String name;
-    double balance;
-    double transactionFee;
+    public double balance;
+    public double transactionFee;
 
-    void deposit(double amount) {
-        // TODO: modify method body
-        balance = balance + amount;
+    public BankAccount() {
+        this.balance = 0;
+        this.transactionFee = 0;
     }
 
-    double getBalance() {
-        return this.balance;
+    public BankAccount(double balance) {
+        this.balance = balance;
+        this.transactionFee = 0;
     }
 
-    boolean withdraw(double amount) {
-        // TODO: modify method body
-        balance = balance - amount;
-        return true;
+    public void deposit(double amount) {
+        if (amount <= 0) return;
+        balance += amount;
     }
 
-    boolean transfer(BankAccount receiver, double amount) {
-        // TODO: modify method body
+    public boolean withdraw(double amount) {
+        if (amount <= 0) return false;
+        if (balance >= amount + transactionFee) {
+            balance -= (amount + transactionFee);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean transfer(BankAccount receiver, double amount) {
+        if (amount <= 0) return false;
+        if (withdraw(amount)) {
+            receiver.deposit(amount);
+            return true;
+        }
         return false;
     }
 }
--- src/main/java/ua/opnu/TimeSpan.java
+++ src/main/java/ua/opnu/TimeSpan.java
@@ -1,46 +1,61 @@
 package ua.opnu;
 
-
 public class TimeSpan {
+    private int hours;
+    private int minutes;
 
-    // TODO: add class fields
-
-    TimeSpan(int hours, int minutes) {
-        // TODO: write constructor body
+    public TimeSpan(int hours, int minutes) {
+        if (hours < 0 || minutes < 0 || minutes > 59) {
+            this.hours = 0;
+            this.minutes = 0;
+        } else {
+            this.hours = hours;
+            this.minutes = minutes;
+        }
     }
 
-    int getHours() {
-        return 0;
+    public int getHours() {
+        return hours;
     }
 
-    int getMinutes() {
-        // TODO: write method body
-        return 0;
+    public int getMinutes() {
+        return minutes;
     }
 
-    void add(int hours, int minutes) {
-        // TODO: write method body
+    public void add(int hours, int minutes) {
+        if (hours < 0 || minutes < 0 || minutes > 59) {
+            return; // игнорируем некорректные значения
+        }
+        this.minutes += minutes;
+        this.hours += hours + this.minutes / 60;
+        this.minutes %= 60;
     }
 
-    void addTimeSpan(TimeSpan timespan) {
-        // TODO: write method body
+    public void addTimeSpan(TimeSpan span) {
+        add(span.getHours(), span.getMinutes());
     }
 
-    double getTotalHours() {
-        // TODO: write method body
-        return 0;
+    public double getTotalHours() {
+        return hours + minutes / 60.0;
     }
 
-    int getTotalMinutes() {
-        // TODO: write method body
-        return 0;
+    public int getTotalMinutes() {
+        return hours * 60 + minutes;
     }
 
-    void subtract(TimeSpan span) {
-        // TODO: write method body
+    public void subtract(TimeSpan span) {
+        int total1 = getTotalMinutes();
+        int total2 = span.getTotalMinutes();
+        if (total2 > total1) return; // не меняем интервал
+        int result = total1 - total2;
+        hours = result / 60;
+        minutes = result % 60;
     }
 
-    void scale(int factor) {
-        // TODO: write method body
+    public void scale(int factor) {
+        if (factor <= 0) return; // игнорируем некорректные значения
+        int total = getTotalMinutes() * factor;
+        hours = total / 60;
+        minutes = total % 60;
     }
 }
